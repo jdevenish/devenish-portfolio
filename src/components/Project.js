7a3d68211479491c957e21ae2192786d8f3c238c
jdevenish <justin.devenish@gmail.com>
2020-05-29 11:47:56 -0400

    Desktop projects section

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,19 +2,92 @@
  * React & Bootstrap Imports
  */
 import React, {useState} from 'react';
-
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+} from 'reactstrap';
 
 /*
  * CSS Files
  */
 import '../scss/Project.scss'
 
-function Project() {
+function Project( { project } ) {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === project.desktopImages.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    };
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? project.desktopImages.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    };
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    };
+
+    const desktopImgs = project.desktopImages.map(img => {
+        return (
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={img._id}
+            >
+                <img src={img.imgURL} alt={img.altId} className="desktopImg"/>
+            </CarouselItem>
+        );
+    });
+
+    const techStackIconsArr = project.techStack.map(icon => {
+        return(
+            <span className="iconify" data-icon={icon.dataIcon} data-inline="false"/>
+        )
+    });
 
     return (
-        <div className="projects-container">
-            <div className="projects--header">
-                <h1>PROJECTS</h1>
+        <div className="project-container">
+            <div className="project--header">
+                <h1>{project.name}</h1>
+            </div>
+            <div className="projectContent--container">
+                <div className="desktopImg--container">
+                    <Carousel
+                        activeIndex={activeIndex}
+                        next={next}
+                        previous={previous}
+                    >
+                        <CarouselIndicators items={project.desktopImages} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                        {desktopImgs}
+                    </Carousel>
+                </div>
+                <div className="project--details">
+
+                    <div>
+                        <h3>Tech Stack</h3>
+                    </div>
+                    <div className="techStack--icons">
+                        {techStackIconsArr}
+                    </div>
+                    <div className="description" >
+                        <h3>Project Overview</h3>
+                        <p>{project.description}</p>
+                    </div>
+                    <div className="btns--container">
+                        <button className="btn btn-primary btn--style">Live Site</button>
+                        <button  className="btn btn-primary btn--style">GitHub Repo</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,14 +11,34 @@ import '../scss/Projects.scss'
 
 function Projects({ projects }) {
 
-    console.log(projects);
+    const dateSortedNewToOld = projects.sort((p1, p2) => {
+       const p1DateArr = p1.date.split("-");
+       const p1Date = new Date(Number(p1DateArr[2]), Number(p1DateArr[0])-1, Number(p1DateArr[1]));
+       const p2DateArr = p2.date.split("-");
+       const p2Date = new Date(Number(p2DateArr[2]), Number(p2DateArr[0])-1, Number(p2DateArr[1]));
+
+       return p1Date - p2Date;
+    });
+
+    const dateSortedOldToNew = projects.sort((p1, p2) => {
+        const p1DateArr = p1.date.split("-");
+        const p1Date = new Date(Number(p1DateArr[2]), Number(p1DateArr[0])-1, Number(p1DateArr[1]));
+        const p2DateArr = p2.date.split("-");
+        const p2Date = new Date(Number(p2DateArr[2]), Number(p2DateArr[0])-1, Number(p2DateArr[1]));
+
+        return p2Date - p1Date;
+    });
+
+    const projectComponents = dateSortedNewToOld.map(project => {
+        return <Project project={project} key={project._id}/>
+    });
 
     return (
         <div className="projects-container">
             <div className="projects--header">
                 <h1>PROJECTS</h1>
             </div>
-
+            {projectComponents}
         </div>
     );
 }
